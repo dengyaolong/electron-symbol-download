5c7ab2b818ae02bcbec30fe4ca823c54a00dfa00
dengyaolong <dengyaolong@yeah.net>
2018-07-01 16:44:34 +0800

    修改返回结果

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-
-const npmview = require('npmview');
-const download = require('download')
+const npmview = require('npmview')
+const _download = require('download')
 const fs = require('fs-extra')
+const chunk = require('chunk')
 const fstream = require('fstream')
 const unzip = require('unzip')
 const path = require('path')
-const sha256File = require('sha256-file');
+const sha256File = require('sha256-file')
 
 
 const MIRROR = 'https://npm.taobao.org/mirrors/electron/'
-const PLATFORMS = ['darwin-x64', 'win32-ia32', 'win32-x64', 'linux-arm', 'linux-arm64', 'linux-armv7l', 'linux-ia32', 'linux-x64', 'mas-x64']
+const PLATFORMS = ['darwin-x64', 'win32-ia32', 'win32-x64', 'linux-arm',
+    'linux-arm64', 'linux-armv7l', 'linux-ia32', 'linux-x64', 'mas-x64']
+function noop () {}
+
+function flat(arr) {
+    return arr.reduce((acc, val) => {
+        return acc.concat(val)
+    }, [])
+}
+
+function download(url, target) { 
+    let writeStream = fs.createWriteStream(target)
+    _download(url).pipe(writeStream);
+    return streamToPromise(writeStream)
+}
+
+function streamToPromise(stream) {
+    return new Promise((resolve, reject) => {
+        stream.on('close', resolve)
+            .on('error', reject)
+    })
+}
 
 function getSymbols(version, pts) {
     let symbols = pts.map(pt =>`electron-v${version}-${pt}-symbols.zip`)
-    return download(`${MIRROR}${version}/SHASUMS256.txt`)
+    return _download(`${MIRROR}${version}/SHASUMS256.txt`)
         .then(data => {
             let lines = data.toString().split('\n')
             let shaAndSymbol = lines
@@ -28,47 +49,44 @@ function getSymbols(version, pts) {
         })
 }
 
-function downloadSymbol({symbol, sha256, version, target}) {
-    let zipTarget = path.resolve(target, symbol)
+async function downloadElectronSymbol({symbol, sha256, version, target, debug=noop}) {
+    let zipPath = path.resolve(target, symbol)
     let downloadPromise;
-    if(fs.existsSync(zipTarget)) {
-        downloadPromise = Promise.resolve()
+    // download
+    if(fs.existsSync(zipPath)) {
+        // downloadPromise = Promise.resolve()
+        debug(symbol, 'zip is existed, do not download again')
     } else {
-        downloadPromise = download(`${MIRROR}${version}/${symbol}`)
-            .then(data => {
-                fs.writeFileSync(zipTarget, data);
-            })
+        let zipUrl =`${MIRROR}${version}/${symbol}`
+        debug('now download symbol zip: ', zipUrl)
+        await download(zipUrl, zipPath)
     }
-    return downloadPromise
-        .then(() => {
-            let theSha256 = sha256File(zipTarget)
-            if(theSha256 === sha256) {
-                // 解压到指定的文件目录
-                return zipTarget
-            } else {
-                fs.removeFileSync(zipTtarget)
-                return Promise.reject('download Failed')
-            }
-        })
-        .then(zipPath => {
-            var readStream = fs.createReadStream(zipPath);
-            let unzipDir = path.resolve(target, symbol.replace(/.zip$/, ''));
-            if(fs.existsSync(unzipDir)) {
-                return unzipDir
-            }
-            fs.ensureDirSync(unzipDir)
-            var writeStream = fstream.Writer(unzipDir);
-            readStream
-              .pipe(unzip.Parse())
-              .pipe(writeStream)
-            return new Promise((resolve, reject) => {
-                writeStream
-                    .on('close', () => {
-                        resolve(unzipDir)
-                    })
-                    .on('error', reject)
-            })
-        })
+
+    // check shasum
+    let theSha256 = sha256File(zipPath)
+    if(theSha256 !== sha256) {
+        fs.removeSync(zipPath)
+        debug('shasum check error')
+        return Promise.reject('download Failed')
+    }
+    
+    // unzip
+    var readStream = fs.createReadStream(zipPath);
+    let unzipDir = path.resolve(target, symbol.replace(/.zip$/, ''));
+    if(fs.existsSync(unzipDir)) {
+        debug(unzipDir, ' dir is existed, do not download again')
+        return unzipDir
+    }
+
+    fs.ensureDirSync(unzipDir)
+    debug('start unzip to', unzipDir)
+    var writeStream = fstream.Writer(unzipDir);
+    readStream
+        .pipe(unzip.Parse())
+        .pipe(writeStream)
+    await streamToPromise(writeStream)
+    debug('end unzip to', unzipDir)
+    return unzipDir
 }
 
 
@@ -85,8 +103,11 @@ function getElectronVersions() {
     })
 }
 
-function downloadElectron({versions, pts=PLATFORMS, target=__dirname}) {
-    if(!versions) return Promise.resolve([])
+function downloadElectronSymbols({versions, pts=PLATFORMS, target=__dirname, debug=noop}) {
+    if(!versions) {
+        debug('versions is required and must be an array of versions')
+        return Promise.resolve([])
+    }
     if(!Array.isArray(versions)) {
         versions = [versions]
     }
@@ -96,22 +117,36 @@ function downloadElectron({versions, pts=PLATFORMS, target=__dirname}) {
             .then(symbols => {
                 return Promise.all(symbols.map(symbol => {
                     symbol.target = target
-                    return downloadSymbol(symbol)
+                    symbol.debug = debug
+                    debug('now download symbol is', JSON.stringify(symbol))
+                    return downloadElectronSymbol(symbol)
                 }))
             })
     }))
+    .then(flat)
 }
 
 function getPts(pts) {
     if(!Array.isArray(pts)) return []
     return pts.filter(pt => PLATFORMS.indexOf(pt) !== -1)
 }
-function downloadAll({pts=PLATFORMS, target}) {
-    return getElectronVersions()
-        .then(versions => {
-            return downloadElectron({versions: versions, pts: pts, target: target})
-        })
+async function downloadAll({pts=PLATFORMS, target, debug=noop, chunkSize=1}) {
+    let allVersions = await getElectronVersions()
+    let chunkedVersions = chunk(allVersions, chunkSize)
+    let res = []
+    for(let i = 0, l = chunkedVersions.length; i < l; i++) {
+        let versions = chunkedVersions[i]
+        debug('will download versions', JSON.stringify(versions))
+        try {
+            let r = await downloadElectronSymbols({versions: versions, pts: pts, target: target})
+            res = res.concat(r)
+            debug('res', res)
+        } catch(e) {
+            debug('error', e)
+        }
+    }
+    return res
 }
 
-module.exports = downloadElectron
+module.exports = downloadElectronSymbols
 module.exports.downloadAll = downloadAll
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 const dl = require('./index.js')
 const path = require('path')
-// dl({versions:['1.8.3'], pts:['darwin-x64', 'win32-ia32']}).then(console.log)
-// dl.downloadAll({pts:['darwin-x64', 'win32-ia32']}).then(console.log)
+dl({versions:['1.8.3'], pts:['darwin-x64', 'win32-ia32'], debug: console.log, target: __dirname}).then(console.log)
+// dl.downloadAll({pts:['darwin-x64', 'win32-ia32'], debug: console.log}).then(console.log)
